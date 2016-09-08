5bf7a3569502a36199fd199e0d3dbf633af645c8
Дуда Владимир <bishop@opz.odessa.ua>
2016-09-08 14:30:32 +0100

    Подчистил аттестованные рабочие места на укр. языке

--- [ОЭ] Аттестованные рабочие места (укр.).sql
+++ [ОЭ] Аттестованные рабочие места (укр.).sql
@@ -1,24 +1,24 @@
 -- TAB = Аттестованные рабочие места (укр.)
-SELECT name_r      "Цех"
-       ,full_name_r "Посада"
-       ,vsego       "Кількість"
-  FROM (SELECT pdr.name_r
-              ,m.full_name_r
-              ,SUM(nvl(s.koli
-                      ,0)) vsego
-              ,-1 female
-              ,'total staff' list_id
-          FROM qwerty.sp_st_pr_zar prop
-              ,qwerty.sp_stat      s
-              ,qwerty.sp_podr      pdr
-              ,qwerty.sp_mest      m
-         WHERE prop.id_prop IN (81
-                               ,82
-                               ,83)
-           AND s.koli <> 0                               
-           AND s.id_stat = prop.id_stat
-           AND pdr.id_cex = s.id_cex
-           AND s.id_mest = m.id_mest
-         GROUP BY pdr.name_r
-                 ,m.full_name_r)
-order by 1, 2
+SELECT decode(GROUPING(pdr.name_r)
+             ,1
+             ,'   АТ "ОПЗ"'
+             ,pdr.name_r) "Цех"
+       ,decode(GROUPING(pdr.name_r)
+             ,1
+             ,'         УCЬОГО:'
+             ,m.full_name_r) "Посада"
+       ,SUM(nvl(s.koli
+              ,0)) "Кількість"
+  FROM qwerty.sp_st_pr_zar prop
+      ,qwerty.sp_stat      s
+      ,qwerty.sp_podr      pdr
+      ,qwerty.sp_mest      m
+ WHERE prop.id_prop IN (SELECT id FROM qwerty.sp_propv_attestation)
+   AND s.koli <> 0
+   AND s.id_stat = prop.id_stat
+   AND pdr.id_cex = s.id_cex
+   AND s.id_mest = m.id_mest
+ GROUP BY ROLLUP((pdr.name_r, m.full_name_r))
+ ORDER BY GROUPING(pdr.name_r)
+         ,1
+         ,2
