412095eaaaa8080dfcdea5a9a2ee3bc8c81f9cb3
Дуда Владимир <bishop@opz.odessa.ua>
2016-09-22 09:22:50 +0100

    Новые запросы за период до 21-го сентября

---
+++ [ОЭ] Графики и смены, которые использывают определенную отметку.sql
@@ -0,0 +1,27 @@
+-- Перечень графиков и смен, которые использывают выбранную отметку
+
+SELECT mark_id
+      ,schedule_id
+      ,schedule_name || ' (смен' || decode(LEVEL
+                                          ,1
+                                          ,'а '
+                                          ,'ы ') || ltrim(sys_connect_by_path(shift_id
+                                                                             ,', ')
+                                                         ,', ') || ')' shifts
+  FROM (SELECT tbls.id_otmetka mark_id
+              ,tbls.id_smen shift_id
+              ,ss.name_u shift_name
+              ,ss.tip_smen schedule_id
+              ,ts.name_u schedule_name
+              ,lag(ss.id_smen) over(PARTITION BY ss.tip_smen ORDER BY ss.id_smen) prev_shift_id
+              ,lead(ss.id_smen) over(PARTITION BY ss.tip_smen ORDER BY ss.id_smen) next_shift_id
+          FROM qwerty.sp_zar_tabl_smen tbls
+              ,qwerty.sp_zar_s_smen    ss
+              ,qwerty.sp_zar_t_smen    ts
+         WHERE tbls.id_otmetka = :m_ID_OTMETKA
+           AND tbls.id_smen = ss.id_smen
+           AND ss.tip_smen = ts.tip_smen)
+ WHERE next_shift_id IS NULL
+CONNECT BY PRIOR schedule_id = schedule_id
+       AND PRIOR shift_id = prev_shift_id
+ START WITH prev_shift_id IS NULL
---
+++ [ОЭ] Работников (из них женщин) на аттестованных рабочих местах, с выбором языка (по штату, по факту, женщин, по спискам).sql
@@ -0,0 +1,175 @@
+--Работники на аттестованных рабочих местах по цехам, из них женщин с выбором языка
+-- с разбивкой по спискам
+-- + количество по факту
+SELECT *
+  FROM (SELECT name_u "Цех"
+               ,full_name_u "Должность"
+               ,SUM(decode(list_id
+                         ,'total staff'
+                         ,vsego
+                         ,0)) "Всего по штату"
+               ,SUM(decode(list_id
+                         ,'total fakt'
+                         ,vsego
+                         ,0)) "Всего по факту"
+               ,SUM(decode(list_id
+                         ,'total fakt'
+                         ,female
+                         ,0)) "Из них женщин"
+               ,SUM(decode(list_id
+                         ,'1'
+                         ,vsego
+                         ,0)) "По списку 1"
+               ,SUM(decode(list_id
+                         ,'1'
+                         ,female
+                         ,0)) "Из них женщин1"
+               ,SUM(decode(list_id
+                         ,'2'
+                         ,vsego
+                         ,0)) "По списку 2"
+               ,SUM(decode(list_id
+                         ,'2'
+                         ,female
+                         ,0)) "Из них женщин2"
+               ,SUM(decode(list_id
+                         ,'12'
+                         ,vsego
+                         ,0)) "По спискам 1 и 2"
+               ,SUM(decode(list_id
+                         ,'12'
+                         ,female
+                         ,0)) "Из них женщин12"
+          FROM (SELECT pdr. &< NAME = "Украинский язык" checkbox = "name_r, name_u" DEFAULT = "name_u" > name_u
+                       ,m.full_ &< NAME = "Украинский язык" > full_name_u
+                       ,SUM(nvl(s.koli
+                              ,0)) vsego
+                       ,-1 female
+                       ,'total staff' list_id
+                  FROM qwerty.sp_st_pr_zar prop
+                      ,qwerty.sp_stat      s
+                      ,qwerty.sp_podr      pdr
+                      ,qwerty.sp_mest      m
+                 WHERE prop.id_prop IN (81
+                                       ,82
+                                       ,83)
+                   AND s.id_stat = prop.id_stat
+                   AND pdr.id_cex = s.id_cex
+                   AND s.id_mest = m.id_mest
+                 GROUP BY pdr.    &< NAME = "Украинский язык" >
+                          ,m.full_ &< NAME = "Украинский язык" >
+                UNION ALL
+                SELECT pdr. &< NAME = "Украинский язык" >
+                       ,m.full_ &< NAME = "Украинский язык" >
+                       ,SUM(decode(nvl(rbk.id_tab
+                                     ,0)
+                                 ,0
+                                 ,0
+                                 ,1)) vsego
+                       ,SUM(decode(osn.id_pol
+                                 ,2
+                                 ,1
+                                 ,0)) female
+                       ,'total fakt' list_id
+                  FROM qwerty.sp_st_pr_zar prop
+                      ,qwerty.sp_stat      s
+                      ,qwerty.sp_rb_key    rbk
+                      ,qwerty.sp_podr      pdr
+                      ,qwerty.sp_ka_osn    osn
+                      ,qwerty.sp_mest      m
+                 WHERE prop.id_prop IN (81
+                                       ,82
+                                       ,83)
+                   AND s.id_stat = prop.id_stat
+                   AND rbk.id_stat = s.id_stat
+                   AND pdr.id_cex = s.id_cex
+                   AND osn.id_tab(+) = rbk.id_tab
+                   AND s.id_mest = m.id_mest
+                 GROUP BY pdr.    &< NAME = "Украинский язык" >
+                          ,m.full_ &< NAME = "Украинский язык" >
+                UNION ALL
+                SELECT pdr. &< NAME = "Украинский язык" >
+                       ,m.full_ &< NAME = "Украинский язык" >
+                       ,SUM(decode(nvl(rbk.id_tab
+                                     ,0)
+                                 ,0
+                                 ,0
+                                 ,1)) vsego
+                       ,SUM(decode(osn.id_pol
+                                 ,2
+                                 ,1
+                                 ,0)) female
+                       ,'1' list
+                  FROM qwerty.sp_st_pr_zar prop
+                      ,qwerty.sp_stat      s
+                      ,qwerty.sp_rb_key    rbk
+                      ,qwerty.sp_podr      pdr
+                      ,qwerty.sp_ka_osn    osn
+                      ,qwerty.sp_mest      m
+                 WHERE prop.id_prop = 81
+                   AND s.id_stat = prop.id_stat
+                   AND rbk.id_stat = s.id_stat
+                   AND pdr.id_cex = s.id_cex
+                   AND osn.id_tab(+) = rbk.id_tab
+                   AND s.id_mest = m.id_mest
+                 GROUP BY pdr.    &< NAME = "Украинский язык" >
+                          ,m.full_ &< NAME = "Украинский язык" >
+                UNION ALL
+                SELECT pdr. &< NAME = "Украинский язык" >
+                       ,m.full_ &< NAME = "Украинский язык" >
+                       ,SUM(decode(nvl(rbk.id_tab
+                                     ,0)
+                                 ,0
+                                 ,0
+                                 ,1)) vsego
+                       ,SUM(decode(osn.id_pol
+                                 ,2
+                                 ,1
+                                 ,0)) female
+                       ,'2' list
+                  FROM qwerty.sp_st_pr_zar prop
+                      ,qwerty.sp_stat      s
+                      ,qwerty.sp_rb_key    rbk
+                      ,qwerty.sp_podr      pdr
+                      ,qwerty.sp_ka_osn    osn
+                      ,qwerty.sp_mest      m
+                 WHERE prop.id_prop = 82
+                   AND s.id_stat = prop.id_stat
+                   AND rbk.id_stat = s.id_stat
+                   AND pdr.id_cex = s.id_cex
+                   AND osn.id_tab(+) = rbk.id_tab
+                   AND s.id_mest = m.id_mest
+                 GROUP BY pdr.    &< NAME = "Украинский язык" >
+                          ,m.full_ &< NAME = "Украинский язык" >
+                UNION ALL
+                SELECT pdr. &< NAME = "Украинский язык" >
+                       ,m.full_ &< NAME = "Украинский язык" >
+                       ,SUM(decode(nvl(rbk.id_tab
+                                     ,0)
+                                 ,0
+                                 ,0
+                                 ,1)) vsego
+                       ,SUM(decode(osn.id_pol
+                                 ,2
+                                 ,1
+                                 ,0)) female
+                       ,'12' list
+                  FROM qwerty.sp_st_pr_zar prop
+                      ,qwerty.sp_stat      s
+                      ,qwerty.sp_rb_key    rbk
+                      ,qwerty.sp_podr      pdr
+                      ,qwerty.sp_ka_osn    osn
+                      ,qwerty.sp_mest      m
+                 WHERE prop.id_prop = 83
+                   AND s.id_stat = prop.id_stat
+                   AND rbk.id_stat = s.id_stat
+                   AND pdr.id_cex = s.id_cex
+                   AND osn.id_tab(+) = rbk.id_tab
+                   AND s.id_mest = m.id_mest
+                 GROUP BY pdr.    &< NAME = "Украинский язык" >
+                          ,m.full_ &< NAME = "Украинский язык" >)
+         GROUP BY name_u
+                 ,full_name_u
+         ORDER BY 1
+                 ,2)
+ WHERE ("Всего по штату" > 0 OR "Всего по факту" > 0)
